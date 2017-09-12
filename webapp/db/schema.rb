09387cdc0f153b5cbd3d0b9162b04c4a94ea93bd
Enrico Fusto <enrico.fusto@pertimm.com>
2017-09-12 18:01:53 +0200

    #15772 Locking strategy for user account is 'failed_attempts'.

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170912145413) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
     t.datetime "created_at", null: false
