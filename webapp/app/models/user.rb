b31cd838defec086667ea7216d615738998800cb
Enrico Fusto <enrico.fusto@pertimm.com>
2017-09-12 17:36:21 +0200

    #15772 Devise installation and first configuration.

---
+++ webapp/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :lockable, :timeoutable
+
+
+end
