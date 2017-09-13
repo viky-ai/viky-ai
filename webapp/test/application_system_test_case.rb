87054451a834199db86c1984dc7dab3c14809fba
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-09-13 17:25:35 +0200

    (changed) #15772 use poltergeist for system tests.

--- webapp/test/application_system_test_case.rb
+++ webapp/test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
+require 'capybara/poltergeist'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :poltergeist
 end
