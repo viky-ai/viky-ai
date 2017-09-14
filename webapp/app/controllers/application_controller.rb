a277efe1eabd014543e8e08af4e32e8f3e747e22
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-09-14 10:23:15 +0200

    (add) #15772 Apply style guide on Devise views.

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:index]
 
-  def hello
+  def index
 
   end
 
--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "application#hello"
+  root to: "application#index"
 
   get 'style-guide', to: 'style_guide#index'
 end
