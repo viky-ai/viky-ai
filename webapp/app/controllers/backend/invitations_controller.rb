b1175ddff84d7f4a80aab955ecf8faf04f784d43
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-09-21 11:21:50 +0200

    #15773 (changed) Simplify devise_invitable integration.

--- webapp/app/controllers/backend/invitations_controller.rb
+++ webapp/app/controllers/backend/invitations_controller.rb
@@ -1,14 +1,24 @@
 class Backend::InvitationsController < Devise::InvitationsController
-  include ApplicationHelper
-
   before_action :authenticate_admin!, only: :new
   layout :switch_layout
+
+  def authenticate_admin!
+    unless (user_signed_in? && current_user.admin?)
+      if user_signed_in?
+        flash[:alert] = t('controllers.backend.application.not_admin')
+        redirect_to root_path
+      else
+        flash[:alert] = t('controllers.backend.application.not_signed_in')
+        redirect_to new_user_session_path
+      end
+    end
+  end
 
 
   private
 
-  def switch_layout
-    ['new', 'create'].include?(action_name) ? "application" : "devise"
-  end
+    def switch_layout
+      ['new', 'create'].include?(action_name) ? "application" : "devise"
+    end
 
 end
