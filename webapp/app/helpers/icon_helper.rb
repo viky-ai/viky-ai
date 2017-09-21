2cb3ecca8f042af938561e1834b4866ec6da169a
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-09-21 14:49:08 +0200

    #15773 (added) Badge components & backend integration.

--- webapp/app/helpers/icon_helper.rb
+++ webapp/app/helpers/icon_helper.rb
@@ -4,6 +4,10 @@ module IconHelper
     '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">  <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>  <path d="M0 0h24v24H0z" fill="none"/></svg>'.html_safe
   end
 
+  def icon_add_circle
+    '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">  <path d="M0 0h24v24H0z" fill="none"/>  <path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>'.html_safe
+  end
+
   def icon_alert
     '<svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>'.html_safe
   end
