16778d71b6afdf3684dead9e13ae3ebb8d8fb70e
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-09-21 16:05:20 +0200

    #15773 (added) Backend users pagination.

--- webapp/app/controllers/backend/users_controller.rb
+++ webapp/app/controllers/backend/users_controller.rb
@@ -3,7 +3,7 @@ class Backend::UsersController < Backend::ApplicationController
   def index
     @search = Backend::UserSearch.new(params[:search])
     @users = User.search(@search.options)
-              .page(params[:page])#.per(1)
+              .page(params[:page])
   end
 
 end
