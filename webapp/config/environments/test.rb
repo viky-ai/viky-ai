860820f9b5335eea2d0075bef0e33039c556f67d
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-09-26 16:13:35 +0200

    #15774 (changed) Remove unused test config params.

--- webapp/config/environments/test.rb
+++ webapp/config/environments/test.rb
@@ -34,8 +34,6 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.active_job.queue_adapter = :test
   config.action_mailer.delivery_method = :test
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the stderr.
