acf4cff65c775fb9459fdaacef87a27613924129
Sebastien Manfredini <sebastien.manfredini@pertimm.com>
2017-10-02 15:12:27 +0200

    #15778 (changed) NLS : adding test for parallelized query NLP in NLS

--- products/bu2_nls/tests/test/nls/test_nls_nlp.rb
+++ products/bu2_nls/tests/test/nls/test_nls_nlp.rb
@@ -7,6 +7,7 @@ module Nls
     @@errorJson   = File.join(@@testDirPath,"package_with_error.json")
     @@goodJson    = File.join(@@testDirPath,"package_without_error.json")
     @@severalpackages  = File.join(@@testDirPath,"several_packages_several_intents.json")
+    @@parallelpackages  = File.join(@@testDirPath,"several_packages_parallelize.json")
 
     def resetDir
       if File.file?(@@errorJson)
@@ -17,6 +18,9 @@ module Nls
       end
       if File.file?(@@severalpackages)
         FileUtils.remove_file(@@severalpackages, force = true)
+      end
+      if File.file?(@@parallelpackages)
+        FileUtils.remove_file(@@parallelpackages, force = true)
       end
       FileUtils.mkdir_p(@@testDirPath)
     end
@@ -44,6 +48,7 @@ module Nls
       FileUtils.cp(fixture_path("package_without_error.json"), @@goodJson)
 
       Nls.stop
+      sleep(0.1)
       Nls.start(@@testDirPath)
 
       response  = RestClient.get(Nls.url_dump)
@@ -53,20 +58,21 @@ module Nls
       [
         {
           "id" => "voqal.ai:datetime",
-           "intents" =>
-           [
-             {
-               "id"=>"0d981484-9313-11e7-abc4-cec278b6b50b",
-               "sentences" => [
-                 {
-                   "sentence" => "Hello Brice",
-                   "locale"=>"fr-FR"
-                 }
-               ]
-             }
-           ]
-         }
-       ];
+          "intents" =>
+          [
+            {
+              "id"=>"0d981484-9313-11e7-abc4-cec278b6b50b",
+              "sentences" =>
+              [
+                {
+                  "sentence" => "Hello Brice",
+                  "locale"=>"fr-FR"
+                }
+              ]
+            }
+          ]
+        }
+      ];
 
       assert_equal expected, actual
       resetDir
@@ -78,6 +84,7 @@ module Nls
       FileUtils.cp(fixture_path("several_packages_several_intents.json"), @@severalpackages)
 
       Nls.stop
+      sleep(0.1)
       Nls.start(@@testDirPath)
 
       param =
@@ -102,7 +109,57 @@ module Nls
         ]
       }
 
+      assert_equal expected, actual
 
+    end
+
+    def test_nlp_nls_parallel_query
+      resetDir
+
+      FileUtils.cp(fixture_path("several_packages_parallelize.json"), @@parallelpackages)
+
+      Nls.stop
+      sleep(0.1)
+      Nls.start(@@testDirPath)
+
+      sentences_array = [];
+
+      for package in 1..10
+        for intent in 1..10
+          for sentence in 1..10
+            sentences_array << ["package_#{package}","intent_#{package}_#{intent}","sentence_#{package}_#{intent}_#{sentence}"]
+          end
+        end
+      end
+
+      tests_number = sentences_array.size
+      Parallel.map(sentences_array, in_threads: 20) do |sentence_array|
+
+        param =
+        {
+          "packages" => [sentence_array[0]],
+          "sentence" => sentence_array[2],
+          "Accept-Language" => "fr-FR"
+        }
+
+        response  = RestClient.post(Nls.url_interpret, param.to_json, content_type: :json)
+        actual    = JSON.parse(response.body)
+
+        expected =
+        {
+          "intents" =>
+          [
+            {
+              "package" => sentence_array[0],
+              "id" => sentence_array[1],
+              "score" => 1
+            }
+          ]
+        }
+
+        assert_equal expected, actual
+
+      end
     end
 
   end
