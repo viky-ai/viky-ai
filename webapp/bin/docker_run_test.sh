9fa1c495f3f5b0f460a469407108f7d329abded6
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-10-03 09:31:31 +0200

    #missing (changed) Simplify CI run test.

--- webapp/bin/docker_run_test.sh
+++ webapp/bin/docker_run_test.sh
@@ -3,11 +3,5 @@ set -x -e
 
 sleep 1
 
-# Setup DB
-./bin/rails db:environment:set
-./bin/rails db:setup db:migrate
-
 # Run tests
-./bin/rails test
-
-./bin/rails test:system
+./bin/rails test test/*
