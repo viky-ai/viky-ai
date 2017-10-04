d9efdfbd024942066df271705cabdfb88321fd65
Brice Ruzand <brice.ruzand@pertimm.com>
2017-10-04 16:56:32 +0200

    #15928 [fixed] X-forward tacking

--- webapp/app/controllers/application_controller.rb
+++ webapp/app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :log_headers
+
+  def log_headers
+    logger.info request.env
+  end
+
 end
