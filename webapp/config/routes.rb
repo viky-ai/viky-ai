284bef69c240e20669b575826c51213e3448006c
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-10-05 10:53:53 +0200

    #15928 (fixed) Access ti Sidekiq UI in production.

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   end
 
   require 'sidekiq/web'
+  Sidekiq::Web.set :sessions, false
   authenticate :user, lambda { |u| u.admin? } do
     mount Sidekiq::Web => '/backend/jobs'
   end
