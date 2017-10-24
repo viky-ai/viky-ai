21e403eaaed9636fa039d2eaca443a1385f10bdc
Enrico Fusto <enrico.fusto@pertimm.com>
2017-10-24 16:40:50 +0200

    #15786 (changed) Api token migration includes old data migration.

--- webapp/db/migrate/20171023071851_add_api_token_to_agents.rb
+++ webapp/db/migrate/20171023071851_add_api_token_to_agents.rb
@@ -1,6 +1,18 @@
 class AddApiTokenToAgents < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :agents, :api_token, :string
     add_index :agents, :api_token, unique: true
+
+    Agent.all.each do |agent|
+      if agent.api_token.nil?
+        agent.ensure_api_token
+        agent.save
+      end
+    end
+  end
+
+  def down
+    remove_index :agents, :api_token
+    remove_column :agents, :api_token
   end
 end
