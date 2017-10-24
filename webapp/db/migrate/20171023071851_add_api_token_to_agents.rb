5e54dc866f7273d220b68821eb2074f8f1825e4e
Enrico Fusto <enrico.fusto@pertimm.com>
2017-10-24 17:38:09 +0200

    #15786 (changed) Agent 'api_token' migration ensures timestamps unchanged.

--- webapp/db/migrate/20171023071851_add_api_token_to_agents.rb
+++ webapp/db/migrate/20171023071851_add_api_token_to_agents.rb
@@ -3,11 +3,16 @@ class AddApiTokenToAgents < ActiveRecord::Migration[5.1]
     add_column :agents, :api_token, :string
     add_index :agents, :api_token, unique: true
 
-    Agent.all.each do |agent|
-      if agent.api_token.nil?
-        agent.ensure_api_token
-        agent.save
+    ActiveRecord::Base.record_timestamps = false
+    begin
+      Agent.all.each do |agent|
+        if agent.api_token.nil?
+          agent.ensure_api_token
+          agent.save!
+        end
       end
+    ensure
+      ActiveRecord::Base.record_timestamps = true
     end
   end
 
