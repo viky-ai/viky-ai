49103ef915d5cbad94783ea860b1a214d1d6d1ef
Olivier Despesse <olivier.despesse@pertimm.com>
2017-11-07 16:06:05 +0100

    #15958 (added) Delete related intents when an agent is destroyed

--- webapp/db/schema.rb
+++ webapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106143706) do
+ActiveRecord::Schema.define(version: 20171107144047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(version: 20171106143706) do
   end
 
   add_foreign_key "agents", "users", column: "owner_id"
-  add_foreign_key "intents", "agents"
+  add_foreign_key "intents", "agents", on_delete: :cascade
   add_foreign_key "memberships", "agents", on_delete: :cascade
   add_foreign_key "memberships", "users", on_delete: :cascade
 end
