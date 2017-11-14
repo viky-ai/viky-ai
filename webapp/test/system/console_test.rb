49ba0d4179f47a22512a073db961e329c333a1c3
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-11-14 10:15:24 +0100

    #15959 (added) Persistant console state through agent navigation!
    Thanks to turbolinks permanent.

--- webapp/test/system/console_test.rb
+++ webapp/test/system/console_test.rb
@@ -2,6 +2,7 @@ require "application_system_test_case"
 
 class ConsoleTest < ApplicationSystemTestCase
 
+
   test 'User index not allowed if user is not logged in' do
     go_to_agents_index
     click_link "My awesome weather bot admin/weather"
@@ -22,5 +23,59 @@ class ConsoleTest < ApplicationSystemTestCase
       assert page.has_content?('intents')
     end
   end
+
+
+  test "console is persisted during agent angivation, operation" do
+    go_to_agents_index
+    click_link "My awesome weather bot admin/weather"
+
+    within('.console') do
+      fill_in 'interpret[sentence]', with: "hello"
+      first('button').click
+      Nlp::Interpret.any_instance.stubs('proceed').returns(
+        {
+          status: 200,
+          body: { intents: [] }
+        }
+      )
+      assert page.has_content?('No intent found.')
+    end
+
+    #
+    # Add intent
+    #
+    click_link 'New intent'
+    within('.modal') do
+      assert page.has_text? 'Create a new intent'
+      fill_in 'ID', with: 'my-new-intent'
+      click_button 'Create'
+    end
+    assert page.has_content?('my-new-intent')
+    assert page.has_content?('No intent found.')
+
+    #
+    # Edit intent
+    #
+    within '.intents-list' do
+      first('.dropdown__trigger > button').trigger('click')
+      click_link 'Configure'
+    end
+
+    within('.modal') do
+      assert page.has_text? 'Edit intent'
+      fill_in 'ID', with: 'my-new-intent-updated'
+      click_button 'Update'
+    end
+    assert page.has_text?('my-new-intent-updated')
+    assert page.has_content?('No intent found.')
+
+    #
+    # Show intent
+    #
+    click_link 'my-new-intent-updated'
+    assert page.has_text?('Expression')
+    assert page.has_content?('No intent found.')
+  end
+
 
 end
