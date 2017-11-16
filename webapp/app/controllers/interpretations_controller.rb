1088d6d6c83b3b6b2a68414218ef1def51ec0aef
Olivier Despesse <olivier.despesse@pertimm.com>
2017-11-16 11:34:56 +0100

    #15958 (changed) Refactor loading of agent, intent and interpretation in controller

--- webapp/app/controllers/interpretations_controller.rb
+++ webapp/app/controllers/interpretations_controller.rb
@@ -1,22 +1,23 @@
 class InterpretationsController < ApplicationController
+  before_action :set_agent
   before_action :check_user_rights
+  before_action :set_intent_and_interpretation, except: [:create]
 
   def create
-    agent = Agent.friendly.find(params[:agent_id])
-    @intent = agent.intents.friendly.find(params[:intent_id])
+    @intent = @agent.intents.friendly.find(params[:intent_id])
     interpretation = Interpretation.new(interpretation_params)
     interpretation.intent = @intent
     @current_locale = interpretation.locale
     respond_to do |format|
       if interpretation.save
         format.js do
-          @html_form = render_to_string(partial: 'form', locals: { intent: @intent, agent: agent, interpretation: Interpretation.new, current_locale: @current_locale })
+          @html_form = render_to_string(partial: 'form', locals: { intent: @intent, agent: @agent, interpretation: Interpretation.new, current_locale: @current_locale })
           @html = render_to_string(partial: 'interpretation', locals: { interpretation: interpretation })
           render partial: 'create_succeed'
         end
       else
         format.js do
-          @html_form = render_to_string(partial: 'form', locals: { intent: @intent, agent: agent, interpretation: interpretation, current_locale: @current_locale })
+          @html_form = render_to_string(partial: 'form', locals: { intent: @intent, agent: @agent, interpretation: interpretation, current_locale: @current_locale })
           render partial: 'create_failed'
         end
       end
@@ -24,32 +25,24 @@ class InterpretationsController < ApplicationController
   end
 
   def show
-    interpretation = Interpretation.find(params[:id])
     respond_to do |format|
       format.js {
-        @interpretation = interpretation
-        @show = render_to_string(partial: 'interpretation', locals: { interpretation: interpretation })
+        @show = render_to_string(partial: 'interpretation', locals: { interpretation: @interpretation })
         render partial: 'show'
       }
     end
   end
 
   def edit
-    interpretation = Interpretation.find(params[:id])
     respond_to do |format|
       format.js {
-        @interpretation = interpretation
-        @form = render_to_string(partial: 'edit.html', locals: { interpretation: interpretation })
+        @form = render_to_string(partial: 'edit.html', locals: { interpretation: @interpretation })
         render partial: 'edit'
       }
     end
   end
 
   def update
-    agent = Agent.friendly.find(params[:agent_id])
-    intent = agent.intents.friendly.find(params[:intent_id])
-    @interpretation = intent.interpretations.find(params[:id])
-
     respond_to do |format|
       if @interpretation.update(interpretation_params)
         format.js {
@@ -66,10 +59,6 @@ class InterpretationsController < ApplicationController
   end
 
   def destroy
-    agent = Agent.friendly.find(params[:agent_id])
-    intent = agent.intents.friendly.find(params[:intent_id])
-    @interpretation = intent.interpretations.find(params[:id])
-
     respond_to do |format|
       if @interpretation.destroy
         format.js { render partial: 'destroy_succeed' }
@@ -81,17 +70,25 @@ class InterpretationsController < ApplicationController
 
   private
 
+    def set_agent
+      @agent = Agent.friendly.find(params[:agent_id])
+    end
+
+    def set_intent_and_interpretation
+      @intent = @agent.intents.friendly.find(params[:intent_id])
+      @interpretation = @intent.interpretations.find(params[:id])
+    end
+
     def interpretation_params
       params.require(:interpretation).permit(:expression, :locale)
     end
 
     def check_user_rights
-      agent = Agent.friendly.find(params[:agent_id])
       case action_name
       when 'show'
-        access_denied unless current_user.can? :show, agent
+        access_denied unless current_user.can? :show, @agent
       when 'create', 'edit', 'update', 'destroy'
-        access_denied unless current_user.can? :edit, agent
+        access_denied unless current_user.can? :edit, @agent
       else
         access_denied
       end
