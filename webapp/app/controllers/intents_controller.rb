9e70fc529501e7c417c28ec68975614cc887aa20
Olivier Despesse <olivier.despesse@pertimm.com>
2017-11-16 11:44:24 +0100

    #15958 (changed) Allow to omit the locale parameter when loading an intent

--- webapp/app/controllers/intents_controller.rb
+++ webapp/app/controllers/intents_controller.rb
@@ -6,7 +6,7 @@ class IntentsController < ApplicationController
 
   def show
     @interpretation = Interpretation.new
-    @current_locale = params[:locale]
+    @current_locale = params[:locale] || @intent.locales.first
   end
 
   def new
