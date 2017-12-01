032208ade8c3c4b9e420fa74bb92005e32d8369e
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-12-01 16:25:52 +0100

    #16262 (changed) Cookie session domain on production based on VOQALAPP_BASEURL

--- webapp/config/initializers/session_store.rb
+++ webapp/config/initializers/session_store.rb
@@ -1,4 +1,8 @@
 Rails.application.config.session_store :cookie_store, key: '_vikyai', domain: {
-  production: URI.parse(ENV["VOQALAPP_BASEURL"]).host,
+  production: URI.parse(ENV.fetch("VOQALAPP_BASEURL") { 'http://www.viky.ai' }).host,
   development: :all
 }.fetch(Rails.env.to_sym, :all)
+
+
+
+
