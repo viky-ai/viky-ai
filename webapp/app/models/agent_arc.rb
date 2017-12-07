e1667bf8eeeb7fc508cbac4db25a1318ffd7d88c
Olivier Despesse <olivier.despesse@pertimm.com>
2017-12-07 09:43:11 +0100

    #16376 (added) Check that a new AgentArc do not create a cycle

--- webapp/app/models/agent_arc.rb
+++ webapp/app/models/agent_arc.rb
@@ -1,4 +1,21 @@
 class AgentArc < ApplicationRecord
   belongs_to :source, foreign_key: "source_id", class_name: "Agent"
   belongs_to :target, foreign_key: "target_id", class_name: "Agent"
+
+  require 'rgl/topsort' # acyclic
+
+  validate :check_acyclic
+
+
+  private
+    def check_acyclic
+      src = source.list_out_arcs
+      dst = target.list_out_arcs
+      graph = RGL::DirectedAdjacencyGraph.new
+      (src+dst).each do |(src, dst)|
+        graph.add_edge(src, dst)
+      end
+      graph.add_edge(source, target)
+      errors.add(:cycle, I18n.t('errors.agent_arc.cycle_detected')) unless graph.acyclic?
+    end
 end
--- webapp/test/models/agent_arc_test.rb
+++ webapp/test/models/agent_arc_test.rb
@@ -104,4 +104,23 @@ class AgentArcTest < ActiveSupport::TestCase
     assert_equal 2, AgentArc.count
   end
 
+
+  test 'Ensure no cycle is present' do
+    #   A
+    #  / \
+    # B   C
+    agent_a = create_agent('Agent A')
+    agent_b = create_agent('Agent B')
+    agent_c = create_agent('Agent C')
+
+    AgentArc.create(source: agent_a, target: agent_b)
+    AgentArc.create(source: agent_c, target: agent_a)
+
+    arc = AgentArc.new(source: agent_b, target: agent_c)
+
+    assert arc.invalid?
+    expected = ['Cycle detected']
+    assert_equal expected, arc.errors.full_messages
+  end
+
 end
