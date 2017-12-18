780d013dc4e4d5d4c02d9bca8c52d6192f2d82dd
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-12-18 15:35:43 +0100

    #16461 (fixed) Try to fix some tests on rancher.

--- webapp/test/models/agent_updated_at_test.rb
+++ webapp/test/models/agent_updated_at_test.rb
@@ -38,8 +38,8 @@ class AgentUpdatedAtTest < ActiveSupport::TestCase
 
     # Interpretation creation
     interpretation = Interpretation.new(
-        expression: 'Good morning John',
-        locale: 'en'
+      expression: 'Good morning John',
+      locale: 'en'
     )
     intent = Intent.find_by_intentname('weather_greeting')
     intent_updated_at = intent.updated_at.to_json
@@ -66,41 +66,46 @@ class AgentUpdatedAtTest < ActiveSupport::TestCase
 
 
   test 'Change updated_at agent date after aliases update' do
+
+    agent = Agent.find_by_agentname('weather')
+    intent = Intent.find_by_intentname('weather_who')
+    interpretation = Interpretation.find_by_expression('Bonjour tout le monde')
+
+    # Interpretation creation
     interpretation_alias = InterpretationAlias.new(
       position_start: 8,
       position_end: 21,
       aliasname: 'who'
     )
-    interpretation = interpretations(:weather_greeting_bonjour)
     interpretation_alias.interpretation = interpretation
-    intent = intents(:weather_who)
-    agent = agents(:weather)
     interpretation_alias.intent = intent
 
-    interpretation_updated_at = interpretation.reload.updated_at.to_json
-    intent_updated_at = intent.reload.updated_at.to_json
-    agent_updated_at = agent.reload.updated_at.to_json
-    sleep 0.1
+    interpretation_updated_at = interpretation.updated_at.to_json
+    intent_updated_at = intent.updated_at.to_json
+    agent_updated_at = agent.updated_at.to_json
     assert interpretation_alias.save
+
     assert_not_equal interpretation_updated_at, interpretation.reload.updated_at.to_json
     assert_not_equal intent_updated_at, intent.reload.updated_at.to_json
     assert_not_equal agent_updated_at, agent.reload.updated_at.to_json
 
-    interpretation_updated_at = interpretation.reload.updated_at.to_json
-    intent_updated_at = intent.reload.updated_at.to_json
-    agent_updated_at = agent.reload.updated_at.to_json
+    # Interpretation update
+    interpretation_updated_at = interpretation.updated_at.to_json
+    intent_updated_at = intent.updated_at.to_json
+    agent_updated_at = agent.updated_at.to_json
     interpretation_alias.aliasname = 'what'
-    sleep 0.1
     assert interpretation_alias.save
+
     assert_not_equal interpretation_updated_at, interpretation.reload.updated_at.to_json
     assert_not_equal intent_updated_at, intent.reload.updated_at.to_json
     assert_not_equal agent_updated_at, agent.reload.updated_at.to_json
 
-    interpretation_updated_at = interpretation.reload.updated_at.to_json
-    intent_updated_at = intent.reload.updated_at.to_json
-    agent_updated_at = agent.reload.updated_at.to_json
-    sleep 0.1
+    # Interpretation destroy
+    interpretation_updated_at = interpretation.updated_at.to_json
+    intent_updated_at = intent.updated_at.to_json
+    agent_updated_at = agent.updated_at.to_json
     assert interpretation_alias.destroy
+
     assert_not_equal interpretation_updated_at, interpretation.reload.updated_at.to_json
     assert_not_equal intent_updated_at, intent.reload.updated_at.to_json
     assert_not_equal agent_updated_at, agent.reload.updated_at.to_json
@@ -115,12 +120,12 @@ class AgentUpdatedAtTest < ActiveSupport::TestCase
 
     arc = AgentArc.create(source: agent_a, target: agent_b)
     assert_not_equal updated_at_a, agent_a.reload.updated_at.to_json
-    assert_equal updated_at_b, agent_b.reload.updated_at.to_json
+    assert_equal     updated_at_b, agent_b.reload.updated_at.to_json
 
-    updated_at_a = agent_a.reload.updated_at.to_json
-    sleep 0.1
+    updated_at_a = agent_a.updated_at.to_json
     assert arc.destroy
+
     assert_not_equal updated_at_a, agent_a.reload.updated_at.to_json
-    assert_equal updated_at_b, agent_b.reload.updated_at.to_json
+    assert_equal     updated_at_b, agent_b.reload.updated_at.to_json
   end
 end
