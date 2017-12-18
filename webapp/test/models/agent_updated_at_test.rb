4f2d249f985c432b5b562bd049edb089ffec4dc2
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2017-12-18 15:14:48 +0100

    #16461 (fixed) Try to fix some tests on rancher.

--- webapp/test/models/agent_updated_at_test.rb
+++ webapp/test/models/agent_updated_at_test.rb
@@ -4,8 +4,6 @@ require 'model_test_helper'
 class AgentUpdatedAtTest < ActiveSupport::TestCase
 
   test 'Change updated_at agent date after intent update' do
-    agent = agents(:weather)
-
     agent = Agent.find_by_agentname('weather')
     updated_at = agent.updated_at.to_json
 
@@ -34,36 +32,36 @@ class AgentUpdatedAtTest < ActiveSupport::TestCase
 
 
   test 'Change updated_at agent date after interpretation update' do
-    agent = agents(:weather)
-    agent_updated_at = agent.reload.updated_at.to_json
+    agent = Agent.find_by_agentname('weather')
 
-    interpretation = Interpretation.new(expression: 'Good morning John', locale: 'en')
-    intent = intents(:weather_greeting)
+    agent_updated_at = agent.updated_at.to_json
+
+    # Interpretation creation
+    interpretation = Interpretation.new(
+        expression: 'Good morning John',
+        locale: 'en'
+    )
+    intent = Intent.find_by_intentname('weather_greeting')
+    intent_updated_at = intent.updated_at.to_json
     interpretation.intent = intent
-    intent_updated_at = intent.reload.updated_at.to_json
-
-    sleep 0.1
     assert interpretation.save
     assert_not_equal intent_updated_at, intent.reload.updated_at.to_json
     assert_not_equal agent_updated_at, agent.reload.updated_at.to_json
 
-    agent_updated_at = agent.reload.updated_at.to_json
-    intent_updated_at = intent.reload.updated_at.to_json
+    # Interpretation update
+    agent_updated_at  = agent.updated_at.to_json
+    intent_updated_at = intent.updated_at.to_json
     interpretation.locale = 'fr'
-    sleep 0.1
     assert interpretation.save
     assert_not_equal intent_updated_at, intent.reload.updated_at.to_json
     assert_not_equal agent_updated_at, agent.reload.updated_at.to_json
 
-    agent_updated_at = agent.reload.updated_at.to_json
-    intent_updated_at = intent.reload.updated_at.to_json
-    sleep 0.1
+    # Interpretation destroy
+    agent_updated_at  = agent.updated_at.to_json
+    intent_updated_at = intent.updated_at.to_json
     assert interpretation.destroy
     assert_not_equal intent_updated_at, intent.reload.updated_at.to_json
     assert_not_equal agent_updated_at, agent.reload.updated_at.to_json
-
-    interpretation = Interpretation.new(expression: 'Good morning John', locale: 'en')
-    interpretation.save
   end
 
 
