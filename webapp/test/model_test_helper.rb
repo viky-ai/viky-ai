b24f1b40c0b9b7c3841eef1c03bdda0853439823
Olivier Despesse <olivier.despesse@pertimm.com>
2018-01-08 15:35:40 +0100

    #16402 (changed) Do not show add dependency button if current user has no edit right

--- webapp/test/model_test_helper.rb
+++ webapp/test/model_test_helper.rb
@@ -1,10 +1,10 @@
 
-def create_agent(name)
+def create_agent(name, user=:admin)
   agent = Agent.new(
     name: name,
     agentname: name.parameterize
   )
-  agent.memberships << Membership.new(user_id: users(:admin).id, rights: 'all')
+  agent.memberships << Membership.new(user_id: users(user).id, rights: 'all')
   assert agent.save
   agent
 end
