15962f50b26723d60f839b3f650cd5ac5aba9d02
Brice Ruzand <brice.ruzand@pertimm.com>
2018-01-12 12:14:58 +0100

    #16616 [change] Dpeloy : improve deployement order (migration, push_all, ...)

--- webapp/bin/docker_run.sh
+++ webapp/bin/docker_run.sh
@@ -3,12 +3,40 @@ set -x -e
 
 # remove previously started server pid
 rm -f ./tmp/pids/server.pid
+rm -f ./tmp/pids/sidekiq.pid
 
-# Setup DB
-./bin/rails db:create db:migrate
 
-# Push all Package
-./bin/rails packages:push_all &
+if [[ "$1" == "master" ]] ; then
 
-# Start web server
-./bin/rails server -b 0.0.0.0 -p 3000
+  # wait for services
+  /usr/local/bin/dockerize -wait tcp://db-postgresql:5432 -wait tcp://db-redis:6379 -timeout 60s
+
+
+  # Setup DB
+  ./bin/rails db:create db:migrate
+
+  # Push all Package
+  ./bin/rails packages:push_all && echo "All packages have been pushed" &
+
+else
+
+  # wait for services
+  # migration can be long
+  /usr/local/bin/dockerize -wait http://app-master:3000 -wait tcp://db-postgresql:5432 -wait tcp://db-redis:6379 -timeout 600s
+
+fi
+
+if [[ "$1" == "worker" ]] ; then
+
+  # Start one worker
+  bundle exec sidekiq -C config/sidekiq.yml
+
+
+else
+
+  echo "viky will be available on ${VIKYAPP_BASEURL}"
+
+  # Start web server
+  ./bin/rails server -b 0.0.0.0 -p 3000
+
+fi
--- webapp/lib/tasks/packages.rake
+++ webapp/lib/tasks/packages.rake
@@ -1,6 +1,10 @@
 namespace :packages do
   desc "Push all agents to NLP"
   task :push_all => [:environment] do |t, args|
+
+    # reduce log level of this task in production
+    Rails.logger.level = :warn if Rails.env.production?
+
     Nlp::Package.push_all
   end
 end
