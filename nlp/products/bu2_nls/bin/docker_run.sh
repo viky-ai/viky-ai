06493c6be19596a693b0bdac483587a6580d9f99
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2018-03-01 12:03:00 +0100

    #16408 (added) Allow osx docker nls run.

--- nlp/products/bu2_nls/bin/docker_run.sh
+++ nlp/products/bu2_nls/bin/docker_run.sh
@@ -23,8 +23,13 @@ if [ "${DOCKER_COMPOSE_DEPLOY}" == "true" ] ; then
   # wait for services
   /usr/local/bin/dockerize -wait tcp://db-redis:6379 -wait http://lb-internal-app:3000 -timeout 600s
 else
-  # wait for services
-  /usr/local/bin/dockerize -wait tcp://localhost:6379 -wait http://localhost:3000 -timeout 60s
+  if [ "${VIKYAPP_BASEURL}" != "" ] ; then
+    # wait for services
+    /usr/local/bin/dockerize -wait tcp://localhost:6379 -wait $VIKYAPP_BASEURL -timeout 60s
+  else
+    # wait for services
+    /usr/local/bin/dockerize -wait tcp://localhost:6379 -wait http://localhost:3000 -timeout 60s
+  fi
 fi
 
 # Start nlp in background, not in daemon
