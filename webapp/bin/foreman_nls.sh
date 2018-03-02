16d83707bd4aace73dc623b1c58f1e6f4894613f
Olivier Despesse <olivier.despesse@pertimm.com>
2018-03-02 14:36:18 +0100

    #missing Fix NLP container errors when env vars are not set

--- webapp/bin/foreman_nls.sh
+++ webapp/bin/foreman_nls.sh
@@ -14,13 +14,21 @@ trap "sigterm_handler; exit" SIGINT
 if [ "$OSX" ]; then
   NETWORK=' -p 9345:9345 '
 else
-  NETWORK=' --net="host" '
+  NETWORK=' --net=host '
+fi
+
+if [ "$VIKYAPP_BASEURL" ]; then
+  PARAM_VIKYAPP_BASEURL=" -e VIKYAPP_BASEURL=$VIKYAPP_BASEURL "
+fi
+
+if [ "$VIKYAPP_REDIS_PACKAGE_NOTIFIER" ]; then
+  PARAM_VIKYAPP_REDIS_PACKAGE_NOTIFIER=" -e VIKYAPP_REDIS_PACKAGE_NOTIFIER=$VIKYAPP_REDIS_PACKAGE_NOTIFIER "
 fi
 
 docker kill foreman_nls || true
 
 docker pull docker-registry.pertimm.net/viky.ai/platform/nlp$1 && \
   docker run \
-    -e VIKYAPP_BASEURL=$VIKYAPP_BASEURL \
-    -e VIKYAPP_REDIS_PACKAGE_NOTIFIER=$VIKYAPP_REDIS_PACKAGE_NOTIFIER \
+    $PARAM_VIKYAPP_BASEURL \
+    $PARAM_VIKYAPP_REDIS_PACKAGE_NOTIFIER \
     ${NETWORK} -t --rm --name foreman_nls --init docker-registry.pertimm.net/viky.ai/platform/nlp$1
