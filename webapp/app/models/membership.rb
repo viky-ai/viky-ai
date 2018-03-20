1477fd1aaff0e0ecdb2012c31efb2e3a726283eb
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2018-03-20 17:31:09 +0100

    #16526 (added) Fragment cache on agent index.

--- webapp/app/models/membership.rb
+++ webapp/app/models/membership.rb
@@ -1,6 +1,6 @@
 class Membership < ApplicationRecord
-  belongs_to :agent
-  belongs_to :user, touch: true
+  belongs_to :agent, touch: true
+  belongs_to :user
 
   validates :rights, inclusion: { in: ['all', 'edit', 'show'] }
   validates :user_id, uniqueness: { scope: [:agent_id] }
