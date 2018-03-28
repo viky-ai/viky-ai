670cad8046eee2dffef09e2fa4aa18f7b54e3795
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2018-03-28 10:36:01 +0200

    #16965 (added) Markdown to HTML

--- webapp/app/models/readme.rb
+++ webapp/app/models/readme.rb
@@ -1,4 +1,16 @@
+require 'html/pipeline'
+
 class Readme < ApplicationRecord
-  belongs_to :agent
+  belongs_to :agent, touch: true
   validates :content, presence: true
+
+  def display
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::MarkdownFilter,
+      HTML::Pipeline::SanitizationFilter,
+      HTML::Pipeline::AutolinkFilter
+    ]
+    result = pipeline.call(content)
+    result[:output].to_s.html_safe
+  end
 end
