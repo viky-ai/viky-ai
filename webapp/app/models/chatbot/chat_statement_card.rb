9fd87411f346c3df3096593eeeed8cd405f4a2e1
Olivier Despesse <olivier.despesse@pertimm.com>
2018-05-04 14:49:50 +0200

    #17255 (changed) Validate a first draft of card statement

--- webapp/app/models/chatbot/chat_statement_card.rb
+++ webapp/app/models/chatbot/chat_statement_card.rb
@@ -3,4 +3,28 @@ class Chatbot::ChatStatementCard
   include Speechable
 
   attr_accessor :components
+
+  validates :components, presence: true, length: { maximum: 5, too_long: I18n.t('errors.chat_statement.card.too_long') }
+  validate :recursive_validation
+
+  private
+
+    def recursive_validation
+      statements = []
+      components.each do |component|
+        case component['nature']
+        when 'text'
+          statements << Chatbot::ChatStatementText.new(component['content'])
+        when 'image'
+          statements << Chatbot::ChatStatementImage.new(component['content'])
+        when 'button'
+          statements << Chatbot::ChatStatementButton.new(component['content'])
+        else
+          errors.add(:base, I18n.t('errors.chat_statement.invalid_nature'))
+        end
+      end
+      statements.find_all(&:invalid?)
+                .each {|statement| errors.add(:base, statement.errors.full_messages.join(', '))}
+    end
+
 end
--- webapp/test/models/chat_statement_test.rb
+++ webapp/test/models/chat_statement_test.rb
@@ -379,4 +379,62 @@ class ChatStatementTest < ActiveSupport::TestCase
 
     assert card_statement.save
   end
+
+
+  test 'Validate card statement' do
+    card_statement = ChatStatement.new(
+      speaker: ChatStatement.speakers[:bot],
+      nature: ChatStatement.natures[:card],
+      content: {
+        components: [{
+          nature: 'text',
+          content: {
+            text: 'The Pertimm logo.'
+          }
+        }, {
+          nature: 'text',
+          content: {
+            text: 'The Pertimm logo.'
+          }
+        }, {
+          nature: 'text',
+          content: {
+            text: 'The Pertimm logo.'
+          }
+        }, {
+          nature: 'text',
+          content: {
+            text: 'The Pertimm logo.'
+          }
+        }, {
+          nature: 'text',
+          content: {
+            text: 'The Pertimm logo.'
+          }
+        }, {
+          nature: 'text',
+          content: {
+            text: 'The Pertimm logo.'
+          }
+        }],
+      },
+      chat_session: chat_sessions(:one)
+    )
+
+    assert card_statement.invalid?
+    assert_equal ['Components is too long (maximum is 5 items)'], card_statement.errors.full_messages
+
+    card_statement.content = { components: [] }
+    assert card_statement.invalid?
+    assert_equal ["Components can't be blank"], card_statement.errors.full_messages
+
+    card_statement.content = { components: [{
+      nature: 'image',
+      content: {
+        url: ''
+      }
+    }] }
+    assert card_statement.invalid?
+    assert_equal ["Url can't be blank"], card_statement.errors.full_messages
+  end
 end
