d93234dfdf3f90820fc17faabe7a2a2d398abd8d
Olivier Despesse <olivier.despesse@pertimm.com>
2018-07-09 10:58:37 +0200

    #17256 (added) Count API with a query parameter

--- webapp/app/models/interpret_request_log.rb
+++ webapp/app/models/interpret_request_log.rb
@@ -2,8 +2,15 @@ class InterpretRequestLog
 
   INDEX_NAME = ['stats-interpret_request_log', Rails.env].join('-').freeze
   INDEX_ALIAS_NAME = ['index', INDEX_NAME].join('-').freeze
+  SEARCH_ALIAS_NAME = ['search', INDEX_NAME].join('-').freeze
 
   attr_reader :id, :timestamp, :sentence, :language
+
+  def self.count(params = {})
+    client = IndexManager.client
+    result = client.count index: SEARCH_ALIAS_NAME, body: params
+    result['count']
+  end
 
   def initialize(timestamp, sentence, language, id = nil)
     @id = id
--- webapp/test/models/interpret_request_log_test.rb
+++ webapp/test/models/interpret_request_log_test.rb
@@ -9,5 +9,27 @@ class InterpretRequestLogTest < ActiveSupport::TestCase
   test 'Save a basic log' do
     log = InterpretRequestLog.new('2018-07-04T14:25:14+02:00', "What 's the weather like ?", 'en')
     assert log.save
+    sleep 1
+    assert_equal 1, InterpretRequestLog.count
+  end
+
+  test 'Count how much interpret request in a specific time frame' do
+    log = InterpretRequestLog.new('2018-07-04T08:00:00+02:00', 'What the weather like today ?', 'en')
+    assert log.save
+    log = InterpretRequestLog.new('2018-07-04T12:00:00+02:00', 'What the weather like tomorrow ?', 'en')
+    assert log.save
+    log = InterpretRequestLog.new('2018-07-04T17:00:00+02:00', 'What the weather like next Sunday ?', 'en')
+    assert log.save
+    sleep 1
+    assert_equal 2, InterpretRequestLog.count(
+      query: {
+        range: {
+          timestamp: {
+            gte: '2018-07-04T10:00:00+02:00',
+            lte: '2018-07-04T18:00:00+02:00'
+          }
+        }
+      }
+    )
   end
 end
