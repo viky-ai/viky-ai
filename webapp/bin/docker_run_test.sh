50157054cd9481c35b06bca3999e05f4695c46be
Mouadh Hsoumi <mouadh.hsoumi@pertimm.com>
2019-01-25 14:19:48 +0100

    #17945 [changed] deploy: changed wait to dockerize

--- webapp/bin/docker_run_test.sh
+++ webapp/bin/docker_run_test.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 set -x -e
 
+source functions.sh
+
 sleep 1
+
+DB_POSTGRES=$(parse_url "$VIKYAPP_DB_HOST")
+DB_REDIS=$(parse_url "$VIKYAPP_ACTIONCABLE_REDIS_URL")
+ES=$(parse_url "$VIKYAPP_STATISTICS_URL")
+KIBANA=$(parse_url "$VIKYAPP_STATISTICS_VISUALIZER_URL")
+
+echo "DB_POSTGRES $DB_POSTGRES"
+echo "DB_REDIS $DB_REDIS"
+echo "ES $ES"
+echo "KIBANA $KIBANA"
+
+# wait for services
+/usr/local/bin/dockerize -wait tcp://$DB_POSTGRES:5432 -wait tcp://$DB_REDIS -wait tcp://$ES -wait tcp://$KIBANA -timeout 60s
 
 # Setup DB
 ./bin/rails db:reset
