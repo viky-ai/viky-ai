2f6063e92364f056f6b585e9a3c4a8e0b47cbd32
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-01-28 15:06:14 +0100

    #18705 (added) VueJS integration.

--- webapp/config/webpack/environment.js
+++ webapp/config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const vue =  require('./loaders/vue')
 const coffee =  require('./loaders/coffee')
 
 // resolve-url-loader must be used before sass-loader
@@ -7,4 +8,5 @@ environment.loaders.get('sass').use.splice(-1, 0, {
 });
 
 environment.loaders.append('coffee', coffee)
+environment.loaders.append('vue', vue)
 module.exports = environment
