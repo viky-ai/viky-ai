9ec1e0bb35defa0530d8a33a1f2563a8049d6c10
Brice Ruzand <brice.ruzand@pertimm.com>
2019-01-28 10:42:43 +0100

    #18705 [changed] NLP notify updated status

--- nlp/nlp_route/lib/nlp_route/package_api_wrapper.rb
+++ nlp/nlp_route/lib/nlp_route/package_api_wrapper.rb
@@ -1,35 +1,34 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 require 'json'
 
 module NlpRoute
-
   class PackageApiWrapper
-
-    VIKY_URL = ENV.fetch("VIKYAPP_BASEURL") { 'http://localhost:3000' }
-    NLP_URL =  ENV.fetch("VIKYAPP_NLP_URL") { 'http://localhost:9345' }
-    VIKYAPP_INTERNAL_API_TOKEN = ENV.fetch("VIKYAPP_INTERNAL_API_TOKEN") { 'Uq6ez5IUdd' }
-    REQUEST_WAIT = 1/200 # 50ms
+    VIKY_URL = ENV.fetch('VIKYAPP_BASEURL') { 'http://localhost:3000' }
+    NLP_URL =  ENV.fetch('VIKYAPP_NLP_URL') { 'http://localhost:9345' }
+    VIKYAPP_INTERNAL_API_TOKEN = ENV.fetch('VIKYAPP_INTERNAL_API_TOKEN') { 'Uq6ez5IUdd' }
+    REQUEST_WAIT = 1 / 200 # 50ms
     FAILURE_THRESHOLD = 3
 
     # list all package from webapp
     def list_id
       puts " + PackageApiWrapper GET  #{VIKY_URL}/api_internal/packages.json"
-      response = RestClient.get "#{VIKY_URL}/api_internal/packages.json" , { content_type: :json, 'Access-Token' => VIKYAPP_INTERNAL_API_TOKEN }
+      response = RestClient.get "#{VIKY_URL}/api_internal/packages.json", content_type: :json, 'Access-Token' => VIKYAPP_INTERNAL_API_TOKEN
       JSON.parse(response.body)
     end
 
     # Get package data json from webapp
     def get_package(id)
       puts " + PackageApiWrapper GET  #{VIKY_URL}/api_internal/packages/#{id}.json"
-      response = RestClient.get "#{VIKY_URL}/api_internal/packages/#{id}.json" , { content_type: :json, 'Access-Token' => VIKYAPP_INTERNAL_API_TOKEN }
-      JSON.parse(response.body)
+      response = RestClient.get "#{VIKY_URL}/api_internal/packages/#{id}.json", content_type: :json, 'Access-Token' => VIKYAPP_INTERNAL_API_TOKEN
+      { version: response.headers['ETag'], data: response.body }
     end
 
     # Send package to NLP
     def update_or_create(id, package)
       puts " + PackageApiWrapper POST #{NLP_URL}/packages/#{id}"
-      RestClient.post "#{NLP_URL}/packages/#{id}", package.to_json
-      true
+      RestClient.post "#{NLP_URL}/packages/#{id}", package[:data]
     end
 
     # delete package to NLP
@@ -38,6 +37,5 @@ module NlpRoute
       RestClient.delete "#{NLP_URL}/packages/#{id}"
       true
     end
-
   end
 end
