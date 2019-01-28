da6d52476e7b7c78b790cadf39edadf7bf3153d0
Olivier Despesse <olivier.despesse@pertimm.com>
2019-01-28 11:35:51 +0100

    #18705 (added) Provide package version when exporting for NLP

--- webapp/app/controllers/api_internal/packages_controller.rb
+++ webapp/app/controllers/api_internal/packages_controller.rb
@@ -9,6 +9,7 @@ class ApiInternal::PackagesController < ApiInternal::ApplicationController
       time = Benchmark.measure do
         @agent = Agent.find(params[:id])
         json = Nlp::Package.new(@agent).generate_json
+        headers['ETag'] = @agent.updated_at
         render json: json
       end
       Rails.logger.info("  Generate package #{@agent.id} - #{@agent.slug} in #{(time.real*1000.0).round(1)} ms")
