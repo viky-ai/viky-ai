253fb0bc1a288461a02874c212c8e52a891804a1
Patrick Constant <patrick.constant@pertimm.com>
2019-02-16 14:03:52 +0100

    #20009 (added) Réduction de la lemmatisation aux seules expressions de la bonne locale.

--- nlp/products/bu2_nls/tests/test/nls/endpoint_interpret/test_lem.rb
+++ nlp/products/bu2_nls/tests/test/nls/endpoint_interpret/test_lem.rb
@@ -21,7 +21,13 @@ module Nls
         travailler << Expression.new("travailler", locale: "fr")
 
         cheval = lem_package.new_interpretation("cheval")
-        cheval << Expression.new("cheval")
+        cheval << Expression.new("cheval", locale: "fr")
+
+        sol = lem_package.new_interpretation("sol")
+        sol << Expression.new("sol", locale: "fr")
+
+        bol = lem_package.new_interpretation("bol")
+        bol << Expression.new("bol")
 
         Nls.package_update(lem_package)
 
@@ -30,11 +36,15 @@ module Nls
 
       def test_lem_simple
 
-        sentence = "travaillerions"
-        check_interpret(sentence, interpretation: "travailler", score: 0.99)
+        check_interpret("travaillerions", interpretation: "travailler", score: 0.99)
+        check_interpret("chevaux", interpretation: "cheval", score: 0.99)
+        check_interpret("sols", interpretation: "sol", score: 0.99)
 
-        sentence = "chevaux"
-        check_interpret(sentence, interpretation: "cheval", score: 0.99)
+        exception = assert_raises Minitest::Assertion do
+          check_interpret("bols", interpretation: "bol", score: 0.99)
+        end
+        assert exception.message.include?("Actual answer did not match on any interpretation")
+
 
       end
 
