01dc774708361d016eac1ca5aeb7429874835fd9
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-04-05 16:36:59 +0200

    #16973 (changed) Improve entities position index migration.

--- webapp/db/migrate/20190403120508_add_index_to_entity_position.rb
+++ webapp/db/migrate/20190403120508_add_index_to_entity_position.rb
@@ -1,11 +1,13 @@
 class AddIndexToEntityPosition < ActiveRecord::Migration[5.1]
   def up
     EntitiesList.find_each do |el|
-      count = el.entities.count
-      position = count
-      el.entities.order('position DESC').find_each do |entity|
-        entity.update_columns(position: position)
-        position = position - 1
+      unless el.entities.select('position').group(:position).having("count(*) > 1").empty?
+        count = el.entities.count
+        position = count
+        el.entities.order('position DESC').each do |entity|
+          entity.update_columns(position: position)
+          position = position - 1
+        end
       end
     end
     add_index :entities, [:entities_list_id, :position], unique: true
