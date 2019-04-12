ccd566943ca27454460533e62a152a517ed89f04
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-04-12 16:46:55 +0200

    #16973 (changed) Extract entities searchable terms from migration to rake.

--- webapp/db/migrate/20190412074505_add_searchable_terms_to_entities.rb
+++ webapp/db/migrate/20190412074505_add_searchable_terms_to_entities.rb
@@ -2,12 +2,9 @@ class AddSearchableTermsToEntities < ActiveRecord::Migration[5.1]
 
   def up
     add_column :entities, :searchable_terms, :text
-    Entity.reset_column_information
-    Entity.find_each do |entity|
-      Rails.logger.silence(Logger::INFO) do
-        entity.update_columns(searchable_terms: Entity.extract_searchable_terms(entity.terms))
-      end
-    end
+    puts "---------------------------------------------------------"
+    puts "Run command: ./bin/rails entities:update_searchable_terms"
+    puts "---------------------------------------------------------"
   end
 
   def down
