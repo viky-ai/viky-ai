ec97ef010c8440f4fab191c65979d4d1a9321b34
Olivier Despesse <olivier.despesse@pertimm.com>
2019-04-15 17:10:24 +0200

    #20421 (changed) Speed up package generation (with buffer)

--- webapp/app/models/json_chunk_encoder.rb
+++ webapp/app/models/json_chunk_encoder.rb
@@ -1,37 +1,53 @@
 class JsonChunkEncoder
 
+  FLUSH_THRESHOLD = 1000
+
   def initialize(io)
     @io = io
     @need_separator = false
+    @buffer = []
   end
 
   def write(options)
-    @io.write(',') if @need_separator
     if options[:object].present?
-      @io.write(options[:object].to_json)
+      @buffer << options[:object].to_json
     else
-      @io.write("\"#{options[:key]}\":\"#{options[:value]}\"")
+      @buffer << "\"#{options[:key]}\":\"#{options[:value]}\""
     end
-    @need_separator = true
+    flush if @buffer.size > FLUSH_THRESHOLD
   end
 
   def wrap_object(key = nil)
+    flush
     @io.write(',') if @need_separator
     @io.write("\"#{key}\":") if key.present?
     @io.write('{')
     @need_separator = false
     yield if block_given?
+    flush
     @io.write('}')
     @need_separator = true
   end
 
   def wrap_array(key = nil)
+    flush
     @io.write(',') if @need_separator
     @io.write("\"#{key}\":") if key.present?
     @io.write('[')
     @need_separator = false
     yield if block_given?
+    flush
     @io.write(']')
     @need_separator = true
   end
+
+  private
+
+    def flush
+      return if @buffer.blank?
+      @io.write(',') if @need_separator
+      @io.write(@buffer.join(','))
+      @buffer = []
+      @need_separator = true
+    end
 end
