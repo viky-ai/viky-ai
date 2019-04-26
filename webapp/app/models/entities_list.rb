ed28e6d28146088a4d811c3fa6a9eee116bbc2e1
Nerisa Chitrakar <nerisa.chitrakar@pertimm.com>
2019-04-26 18:02:54 +0200

    #20505 [added] webapp: Add entities extract from db in batch method
    -from #20421

--- webapp/app/models/entities_list.rb
+++ webapp/app/models/entities_list.rb
@@ -82,6 +82,24 @@ class EntitiesList < ApplicationRecord
     @proximity ||= ExpressionProximity.new(read_attribute(:proximity))
   end
 
+  def entities_in_ordered_batchs(batch_size = 1_000)
+    Enumerator.new(entities.count) do |block|
+      if entities.exists?
+        cursor_max = entities.select(:position).order(position: :desc).first.position
+        cursor_min = entities.select(:position).order(position: :desc).last.position
+        cursor = cursor_max + 1
+        while cursor > cursor_min do
+          batch = entities.where('position < ?', cursor).order(position: :desc).limit(batch_size)
+          block << batch
+          cursor = batch.last.position
+        end
+      else
+        block << []
+      end
+    end
+  end
+
+
   private
 
     def clean_listname
