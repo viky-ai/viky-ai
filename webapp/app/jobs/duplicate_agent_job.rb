c8e9b6141e143355564dc5324bd6b03738058c45
Nerisa Chitrakar <nerisa.chitrakar@pertimm.com>
2019-04-29 17:46:07 +0200

    #20505 (missing) webapp: Remove test measurement code

--- webapp/app/jobs/duplicate_agent_job.rb
+++ webapp/app/jobs/duplicate_agent_job.rb
@@ -7,20 +7,16 @@ class DuplicateAgentJob < ApplicationJob
   retry_on ActiveRecord::Deadlocked
 
   def perform(agent, current_user)
-    print_memory_usage do
-      print_time_spent do
-        new_agent = AgentDuplicator.new(agent, current_user).duplicate
-        if new_agent.errors.any?
-          UserNotificationsChannel.broadcast_to current_user,
-            alert: I18n.t(
-              'views.agents.duplication.failed_message',
-              errors: new_agent.errors.full_messages.join(', ')
-            )
-        else
-          UserNotificationsChannel.broadcast_to current_user,
-            notice: I18n.t('views.agents.duplication.success_message', name: new_agent.name)
-        end
-      end
+    new_agent = AgentDuplicator.new(agent, current_user).duplicate
+    if new_agent.errors.any?
+      UserNotificationsChannel.broadcast_to current_user,
+        alert: I18n.t(
+          'views.agents.duplication.failed_message',
+          errors: new_agent.errors.full_messages.join(', ')
+        )
+    else
+      UserNotificationsChannel.broadcast_to current_user,
+        notice: I18n.t('views.agents.duplication.success_message', name: new_agent.name)
     end
   end
 end
