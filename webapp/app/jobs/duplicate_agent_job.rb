61d63e3fe4d15c9f6d41961b39b1fc741ccfefc1
Nerisa Chitrakar <nerisa.chitrakar@pertimm.com>
2019-04-29 12:03:19 +0200

    #20505 [added] webapp: Check for errors in new agent

--- webapp/app/jobs/duplicate_agent_job.rb
+++ webapp/app/jobs/duplicate_agent_job.rb
@@ -1,3 +1,4 @@
+require_relative 'measure_helper.rb'
 class DuplicateAgentJob < ApplicationJob
   queue_as :default
 
@@ -6,17 +7,20 @@ class DuplicateAgentJob < ApplicationJob
   retry_on ActiveRecord::Deadlocked
 
   def perform(agent, current_user)
-    new_agent = AgentDuplicator.new(agent, current_user).duplicate
-    if new_agent.valid?
-      new_agent.save!
-      UserNotificationsChannel.broadcast_to current_user,
-        notice: I18n.t('views.agents.duplication.success_message', name: new_agent.name)
-    else
-      UserNotificationsChannel.broadcast_to current_user,
-        alert: I18n.t(
-          'views.agents.duplication.failed_message',
-          errors: new_agent.errors.full_messages.join(', ')
-        )
+    print_memory_usage do
+      print_time_spent do
+        new_agent = AgentDuplicator.new(agent, current_user).duplicate
+        if new_agent.errors.any?
+          UserNotificationsChannel.broadcast_to current_user,
+            alert: I18n.t(
+              'views.agents.duplication.failed_message',
+              errors: new_agent.errors.full_messages.join(', ')
+            )
+        else
+          UserNotificationsChannel.broadcast_to current_user,
+            notice: I18n.t('views.agents.duplication.success_message', name: new_agent.name)
+        end
+      end
     end
   end
 end
