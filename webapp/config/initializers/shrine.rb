8c37ab3e3638ed4101d31a36cb9d955e4e430fd1
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-08-30 17:30:12 +0200

    #20904 (changed) Update Shrine gem.

--- webapp/config/initializers/shrine.rb
+++ webapp/config/initializers/shrine.rb
@@ -2,10 +2,9 @@ require "shrine"
 require "shrine/storage/file_system"
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"), # permanent
+  cache: Shrine::Storage::FileSystem.new(Rails.root.join("public"), prefix: "uploads/cache"), # temporary
+  store: Shrine::Storage::FileSystem.new(Rails.root.join("public"), prefix: "uploads/store"), # permanent
 }
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
-Shrine.plugin :logging, logger: Rails.logger
