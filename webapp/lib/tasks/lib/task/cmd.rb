1ee2e60acb16b7266f192bbd11e15b1e8f0dc36e
Olivier Despesse <olivier.despesse@pertimm.com>
2019-09-05 17:50:29 +0200

    #20979 (changed) Fix unnecessary line break on command output

--- webapp/lib/tasks/lib/task/cmd.rb
+++ webapp/lib/tasks/lib/task/cmd.rb
@@ -8,7 +8,7 @@ module Task::Cmd
       if opts[:capture_output]
         stdout_and_stderr.each { |line| data << line }
       else
-        stdout_and_stderr.each { |line| Task::Print.substep "⤷ #{line}" }
+        stdout_and_stderr.each { |line| Task::Print.substep "⤷ #{line.rstrip}" }
       end
 
       # Process::Status object returned.
@@ -16,7 +16,7 @@ module Task::Cmd
 
       unless exit_status.success?
         Task::Print.step cmd
-        data.each { |line| Task::Print.substep "⤷ #{line}" } if opts[:capture_output]
+        data.each { |line| Task::Print.substep "⤷ #{line.rstrip}" } if opts[:capture_output]
         raise "Command \"#{cmd}\" failed"
       end
     end
