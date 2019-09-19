885b2e541a7bf313f595510bcf4c47a974b6d13b
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-09-19 14:18:28 +0200

    #20979 (changed) webapp does not use active storage, action mailbox or action text for the time being

--- webapp/config/application.rb
+++ webapp/config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
+
 require 'csv'
 
 # Require the gems listed in Gemfile, including any gems
