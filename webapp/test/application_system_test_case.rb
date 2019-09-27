f82a44c1b1d30c976384c417045d6f71cac8d5ef
Olivier Despesse <olivier.despesse@pertimm.com>
2019-09-27 16:02:29 +0200

    #20979 (changed) Disable shared memory because we consume too much for Docker on Gitlab (max default is 64 MB)

--- webapp/test/application_system_test_case.rb
+++ webapp/test/application_system_test_case.rb
@@ -7,6 +7,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.register_driver(:headless_chrome) do |app|
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument 'no-sandbox'
+    options.add_argument 'disable-dev-shm-usage'
     options.headless!
     driver_options = { browser: :chrome, options: options }
 
