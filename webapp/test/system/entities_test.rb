b8fc39215407206b6a736215e885405e1a0a0d7a
Jean-Marie Vallet <jean-marie.vallet@pertimm.com>
2019-10-09 09:21:47 +0200

    #20979 (changed) Reorganize entities system tests.

--- webapp/test/system/entities_test.rb
+++ webapp/test/system/entities_test.rb
@@ -273,6 +273,7 @@ class EntitiesTest < ApplicationSystemTestCase
 
   test "Keep solution after unchecking checkbox" do
     admin_go_to_entities_list_show(agents(:weather), entities_lists(:weather_conditions))
+
     within(".entity-form") do
       uncheck("Auto solution")
       execute_script %Q{ $("#terms__new_entity").trigger("click") }
@@ -288,95 +289,6 @@ class EntitiesTest < ApplicationSystemTestCase
       assert has_css?(".entity-form")
       assert has_text?('"condition: brumeux"')
     end
-  end
-
-
-  test "Export entities" do
-    admin_go_to_entities_list_show(agents(:weather), entities_lists(:weather_conditions))
-    within ".header" do
-      assert has_link? "Export"
-    end
-  end
-
-
-  test "Import entities hidden when no edit right" do
-    login_as "show_on_agent_weather@viky.ai", "BimBamBoom"
-    visit user_agent_entities_list_path(users(:admin), agents(:weather), entities_lists(:weather_conditions))
-    within ".header" do
-      assert has_no_link? "Import"
-    end
-  end
-
-
-  test "Import entities without file" do
-    admin_go_to_entities_list_show(agents(:weather), entities_lists(:weather_conditions))
-    click_link "Import"
-    within(".modal") do
-      assert has_text?("Import entities")
-      assert has_no_text?("File must be present")
-      click_button "Import"
-      assert has_text?("File must be present")
-    end
-  end
-
-
-  test "Import entities falling" do
-    admin_go_to_entities_list_show(agents(:weather), entities_lists(:weather_conditions))
-    assert_equal 2, all("#entities-list > li").count
-    click_link "Import"
-
-    perform_enqueued_jobs do
-      within(".modal") do
-        assert has_text?("Import entities")
-        file = build_fixture_files_path('import_entities_ko.csv')
-        attach_file("import_file", file, make_visible: true)
-        click_button "Import"
-      end
-      assert has_text?("Import failed")
-    end
-  end
-
-
-  test "Import append entities with success" do
-    admin_go_to_entities_list_show(agents(:weather), entities_lists(:weather_conditions))
-    assert_equal 2, all("#entities-list > li").count
-    click_link "Import"
-
-    perform_enqueued_jobs do
-      within(".modal") do
-        assert has_text?("Import entities")
-        file = build_fixture_files_path('import_entities_ok.csv')
-        attach_file("import_file", file, make_visible: true)
-        click_button "Import"
-      end
-      assert has_text?("Processing import")
-      assert has_text?("3 entities imported successfully")
-    end
-    # Wait redirection
-    sleep 6
-    assert has_css?("#entities-list > li", count: 5)
-  end
-
-
-  test "Import replace entities  with success" do
-    admin_go_to_entities_list_show(agents(:weather), entities_lists(:weather_conditions))
-    assert_equal 2, all("#entities-list > li").count
-    click_link "Import"
-
-    perform_enqueued_jobs do
-      within(".modal") do
-        assert has_text? "Import entities"
-        file = build_fixture_files_path('import_entities_ok.csv')
-        attach_file("import_file", file, make_visible: true)
-        choose "Replace current entities"
-        click_button "Import"
-      end
-      assert has_text? "Processing import"
-      assert has_text? "3 entities imported successfully"
-    end
-    # Wait redirection
-    sleep 6
-    assert has_css?("#entities-list > li" ,count: 3)
   end
 
 
