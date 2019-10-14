885c8ddf21d03ac2b1c2a373bb29af1f40fd84be
Brice Ruzand <brice.ruzand@pertimm.com>
2019-10-14 11:04:33 +0200

    #17945 [added] Backup restore on S3

--- webapp/app/models/interpret_request_log_client.rb
+++ webapp/app/models/interpret_request_log_client.rb
@@ -19,7 +19,6 @@ class InterpretRequestLogClient
     raise "Missing statistics configuration for environment #{environment}" if config.empty?
 
     @client = Elasticsearch::Client.new(config[:client].symbolize_keys.merge(options))
-    @repository_name = 'viky-es-backup_dev'
     @expected_cluster_status = Rails.env.production? ? 'green' : 'yellow'
     @refresh_on_save = false
   end
@@ -202,36 +201,47 @@ class InterpretRequestLogClient
     list_indices.each { |index| delete_index index }
   end
 
-  def create_repository(location)
-    @client.snapshot.create_repository repository: @repository_name, body: {
+  def create_local_repository(repository_name, env_name)
+    @client.snapshot.create_repository repository: repository_name, body: {
       type: 'fs',
-      settings: { location: location }
+      settings: { location: "/backup_data/#{env_name}/es-backup" }
     }
   end
 
-  def delete_repository
-    @client.snapshot.delete_repository repository: @repository_name
+  def create_s3_restore_repository(repository_name, env_name, bucket = 'viky-ai')
+    @client.snapshot.create_repository repository: repository_name, body: {
+      type: 's3',
+      settings: {
+        bucket: bucket,
+        readonly: true,
+        base_path: "viky-backups/#{env_name}/es-backup"
+      }
+    }
   end
 
-  def create_snapshot(snapshot_name)
-    @client.snapshot.create repository: @repository_name, snapshot: snapshot_name, wait_for_completion: true, body: {
+  def create_snapshot(repository_name, snapshot_name)
+    @client.snapshot.create repository: repository_name, snapshot: snapshot_name, wait_for_completion: true, body: {
       indices: "#{InterpretRequestLogClient.index_name}-*",
-      include_global_state: false
+      include_global_state: false,
+      ignore_unavailable: true
     }
   end
 
-  def restore_most_recent_snapshot
+  def restore_most_recent_snapshot(repository)
     snapshot = @client.cat.snapshots(
-      repository: @repository_name,
+      repository: repository,
       s: 'end_epoch',
       h: 'id'
     ).split("\n").last
     @client.snapshot.restore(
-      repository: @repository_name,
+      repository: repository,
       snapshot: snapshot,
       wait_for_completion: true,
       body: {
         indices: "#{InterpretRequestLogClient.index_name}-*",
+        include_aliases: true,
+        allow_no_indices: true,
+        ignore_unavailable: true,
         index_settings: {
           'index.number_of_replicas' => 1
         }
