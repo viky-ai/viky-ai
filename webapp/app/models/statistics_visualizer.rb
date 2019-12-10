1f10b386db4fa3176ed60bd8855a74cf3d9182ae
Brice Ruzand <brice.ruzand@pertimm.com>
2019-12-10 17:53:43 +0100

    #15 [change] Kibana : use new export/import format in ndjson

--- webapp/app/models/statistics_visualizer.rb
+++ webapp/app/models/statistics_visualizer.rb
@@ -1,47 +1,27 @@
+# frozen_string_literal: true
+
 require 'net/http'
 
 class StatisticsVisualizer
-
-  JSON_HEADERS = { 'Content-Type' => 'application/json', 'kbn-xsrf' => 'true' }
-
-  def initialize
-    uri = URI.parse(endpoint)
-    @client = Net::HTTP.new(uri.host, uri.port)
-  end
-
   def endpoint
-    ENV.fetch('VIKYAPP_STATISTICS_VISUALIZER_URL') { 'http://localhost:5601' }
+    ENV.fetch('VIKYAPP_STATISTICS_VISUALIZER_URL') do
+      'http://localhost:5601/kibana'
+    end
   end
 
   def configure
-    kibana_configurations = fetch_configurations
-    result = save(clean(kibana_configurations))
-    if result.code != '200'
-      raise "Error while saving Kibana configuration : #{result.body}"
-    end
-  end
-
-  private
-    def fetch_configurations
-      config_file = "#{Rails.root}/config/kibana/kibana-conf.json"
-      JSON.parse(ERB.new(File.read(config_file)).result)
-    end
-
-    def save(parameters)
-      path = '/kibana/api/saved_objects/_bulk_create?overwrite=true'
-      @client.post(path, parameters.to_json, JSON_HEADERS)
-    end
-
-    def clean(configurations)
-      configurations.map do |configuration|
-        configuration['id'] = configuration['_id']
-        configuration.delete '_id'
-        configuration['type'] = configuration['_type']
-        configuration.delete '_type'
-        configuration['attributes'] = configuration['_source']
-        configuration.delete '_source'
-        configuration.delete  '_migrationVersion'
-        configuration
+    file = "#{Rails.root}/config/kibana/kibana-conf.ndjson"
+    uri = URI("#{endpoint}/api/saved_objects/_import?overwrite=true")
+    request = Net::HTTP::Post.new(uri)
+    request['kbn-xsrf'] = true
+    form_data = [['file', File.open(file), { filename: File.basename(file) }]]
+    request.set_form(form_data, 'multipart/form-data')
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      response = http.request(request)
+      if response.code != '200'
+        raise "Error while saving Kibana configuration : #{response.body}"
       end
     end
+    true
+  end
 end
