0b07bf18b066cf69f06b3376438b8b504a7a64ef
Olivier Despesse <olivier.despesse@pertimm.com>
2020-06-24 16:44:00 +0200

    #84 Fix Chrome regression ignoring the disable shared memory option

--- webapp/test/application_system_test_case.rb
+++ webapp/test/application_system_test_case.rb
@@ -7,8 +7,9 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
 
   Capybara.register_driver(:headless_chrome) do |app|
     options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument 'no-sandbox'
-    options.add_argument 'disable-dev-shm-usage'
+    options.add_argument '--no-sandbox'
+    options.add_argument '--disable-dev-shm-usage'
+    options.add_argument '--disable-gpu'
     options.headless!
     driver_options = { browser: :chrome, options: options }
     driver_options[:url] = ENV['SELENIUM_REMOTE_URL'] if ENV['SELENIUM_REMOTE_URL']
